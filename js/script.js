8134aa7e2a4b1acfd0336ce805ecfd306a983c20
patiberzin <pberzincand@gmail.com>
2022-05-07 18:32:55 -0300

    modificacoes

--- js/script.js
+++ js/script.js
@@ -1,11 +1,23 @@
-const linksInternos = document.querySelectorAll('.js-menu a[href^="#"]');
+function scrollSuave() {
 
-function scrollToSection(event) {
-    event.preventDefault();
+    const linksInternos = document.querySelectorAll('.js-menu a[href^="#"]');
+
+    function scrollToSection(event) {
+        event.preventDefault();
+        const href = event.currentTarget.getAttribute('href');
+        const section = document.querySelector(href);
+
+        section.scrollIntoView({
+            behavior: 'smooth',
+            block: 'start',
+        });
+    }
+
+    linksInternos.forEach((link) => {
+        link.addEventListener('click', scrollToSection);
+    });
 }
+scrollSuave();
 
-linksInternos.forEach((link) => {
-    link.addEventListener('click', scrollToSection);
-});
 
 
