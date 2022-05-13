46004419e72e2f032ad853c6b1b24f40c710d9bd
patiberzin <pberzincand@gmail.com>
2022-05-13 14:46:52 -0300

    ajustes

--- script.js
+++ script.js
@@ -23,19 +23,21 @@ scrollSuave();
 
 function initAccordion() {
     const accordionList = document.querySelectorAll('.js-accordion dt');
-    accordionList[0].classList.add('ativo');
-    accordionList[0].nextElementSibling.classList.add('ativo');
-    
-    function activeAccordion() {
-        this.classList.toggle('ativo');
-        this.nextElementSibling.classList.toggle('ativo');
-    }
-    
-    accordionList.forEach((item) => {
-        item.addEventListener('click', activeAccordion);
-    });
-}
+    const activeClass = 'ativo';
+    if (accordionList.length) {
+        accordionList[0].classList.add(activeClass);
+        accordionList[0].nextElementSibling.classList.add(activeClass);
 
+        function activeAccordion() {
+            this.classList.toggle(activeClass);
+            this.nextElementSibling.classList.toggle(activeClass);
+        }
+
+        accordionList.forEach((item) => {
+            item.addEventListener('click', activeAccordion);
+        });
+    }
+}
 initAccordion();
 
 
