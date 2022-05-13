a6ddbdbe9f0e03a9086c1392be3dbb97b717d27e
patiberzin <pberzincand@gmail.com>
2022-05-13 14:29:32 -0300

    anima accordion

--- script.js
+++ script.js
@@ -21,15 +21,22 @@ scrollSuave();
 
 //Accordeon List pg UX/UI
 
-const accordionList = document.querySelectorAll('.js-accordion dt');
-
-function activeAccordion() {
-    this.classList.toggle('ativo');
-    this.nextElementSibling.classList.toggle('ativo');
+function initAccordion() {
+    const accordionList = document.querySelectorAll('.js-accordion dt');
+    accordionList[0].classList.add('ativo');
+    accordionList[0].nextElementSibling.classList.add('ativo');
+    
+    function activeAccordion() {
+        this.classList.toggle('ativo');
+        this.nextElementSibling.classList.toggle('ativo');
+    }
+    
+    accordionList.forEach((item) => {
+        item.addEventListener('click', activeAccordion);
+    });
 }
 
-accordionList.forEach((item) => {
-    item.addEventListener('click', activeAccordion);
-});
+initAccordion();
+
 
 
